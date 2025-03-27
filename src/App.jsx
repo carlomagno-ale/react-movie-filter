cd373a0dd2a3782ef55e5336dc9acfec176d007b
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-27 16:47:49 +0100

    Add movie list and enhance App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import moviesList from './data/movies.js';
 import { useState } from 'react'
 
 export default function App() {
 
+  console.log(moviesList)
+
+  const [movies, setMovies] = useState(moviesList)
+
+  console.log(movies)
+  console.log(setMovies)
+
   return (
     <>
-      <h1>Movies</h1>
+      <div className="container">
+        <h1 className='text-center'>Movies</h1>
+
+        <div className='d-flex justify-content-center'>
+          <form>
+            <input
+              type="text"
+              className="form-control"
+              name="new_article"
+              id="new_article"
+              placeholder="Cerca il titolo del film"
+            />
+          </form>
+
+        </div>
+
+        <div className='row my-4'>
+          {movies.map((movie, index) => (
+            <div className='col-4 mt-2' key={index}>
+              <div className="card" style={{ width: '15rem' }}>
+                <img
+                  className="card-img-top"
+                  src={movie.img}
+                  alt="random"
+                />
+                <div className="card-body">
+                  <h5 className="card-title fs-6">{movie.title}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/data/movies.js
@@ -0,0 +1,10 @@
+const moviesList = [
+    { title: 'Inception', genre: 'Fantascienza', img: 'https://picsum.photos/200/200.jpg' },
+    { title: 'Il Padrino', genre: 'Thriller', img: 'https://picsum.photos/200/200.jpg' },
+    { title: 'Titanic', genre: 'Romantico', img: 'https://picsum.photos/200/200.jpg' },
+    { title: 'Batman', genre: 'Azione', img: 'https://picsum.photos/200/200.jpg' },
+    { title: 'Interstellar', genre: 'Fantascienza', img: 'https://picsum.photos/200/200.jpg' },
+    { title: 'Pulp Fiction', genre: 'Thriller', img: 'https://picsum.photos/200/200.jpg' },
+]
+
+export default moviesList
